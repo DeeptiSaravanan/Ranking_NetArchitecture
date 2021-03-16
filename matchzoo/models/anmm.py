4e61037d7122f2fd855719555d798cfe44741021
Deep98 <deep02matrix@gmail.com>
2021-03-17 00:45:47 +0530

    New Updated Architecture

---
+++ matchzoo/models/anmm.py
@@ -0,0 +1,122 @@
+"""An implementation of aNMM Model."""
+
+import matchzoo as mz
+import keras
+from keras.activations import softmax
+from keras.initializers import RandomUniform
+
+from matchzoo.engine.base_model import BaseModel
+from matchzoo.engine.param import Param
+from matchzoo.engine.param_table import ParamTable
+from matchzoo.engine import hyper_spaces
+
+#ANMM
+class ANMM(BaseModel):
+    """
+    ANMM Model.
+
+    Examples:
+        >>> model = ANMM()
+        >>> model.guess_and_fill_missing_params(verbose=0)
+        >>> model.build()
+
+    """
+
+    @classmethod
+    def get_default_params(cls) -> ParamTable:
+        """:return: model default parameters."""
+        params = super().get_default_params(with_embedding=True)
+        params.add(Param(
+            name='dropout_rate', value=0.1,
+            desc="The dropout rate.",
+            hyper_space=hyper_spaces.quniform(0, 1, 0.05)
+        ))
+        params.add(Param(
+            name='num_layers', value=2,
+            desc="Number of hidden layers in the MLP "
+                 "layer."
+        ))
+        params.add(Param(
+            name='hidden_sizes', value=[30, 30],
+            desc="Number of hidden size for each hidden"
+                 " layer"
+        ))
+        return params
+
+    def build(self):
+        """
+        Build model structure.
+
+        aNMM model based on bin weighting and query term attentions
+        """
+        # query is [batch_size, left_text_len]
+        # doc is [batch_size, right_text_len, bin_num]
+        query, doc = self._make_inputs()
+        embedding = self._make_embedding_layer()
+        
+        print('Printing doc output here .......')
+        print(type(doc))
+        print(doc)
+
+        q_embed0 = embedding(query)
+        
+        d_bin0 = keras.layers.Dropout(
+            rate=self._params['dropout_rate'])(doc)
+            
+        #frequency vector
+        
+        freq_vec = np.ones(d_bin0.shape)
+        
+        d_bin1 = keras.layers.Dropout(
+            rate=self._params['dropout_rate'])(freq_vec)
+            
+        dbin = keras.layers.Concatenate()([d_bin0, d_bin1])
+        
+        for layer_id in range(self._params['num_layers'] - 1):
+            d_bin = keras.layers.Dense(
+                self._params['hidden_sizes'][layer_id],
+                kernel_initializer=RandomUniform())(d_bin)
+            d_bin = keras.layers.Activation('tanh')(d_bin)
+        
+        d_bin = keras.layers.Dense(
+            self._params['hidden_sizes'][self._params['num_layers'] - 1])(
+            d_bin)
+        one_vec = np.ones(d_bin.shape)
+        
+        one_tensors = tf.convert_to_tensor(one_vec)
+        
+        d_one_tensors = keras.layers.Reshape((d_bin,))(one_tensors)
+        q_one_tensors = keras.layers.Reshape((q_attention,))(one_tensors)
+        
+        # Score 1
+        score0 = keras.layers.Dot(axes=[1, 1])([d_bin, d_one_tensors])
+        x_out0 = self._make_output_layer()(score0)
+        
+        pos_vec = x_out0
+        x_out0 = x_out0.sort(reverse=True)
+        piter=0
+        for positionv in pos_vec:
+        				pind = x_out0.index(positionv)
+        				pos_vec[piter] = 1/float(pind)
+        				piter = piter + 1
+        
+        q_embed1 = keras.layers.Dropout(
+            rate=self._params['dropout_rate'])(pos_vec)
+        
+        q_embed = keras.layers.Concatenate()([q_embed0, q_embed1])
+        
+        q_attention = keras.layers.Dense(
+            1, kernel_initializer=RandomUniform(), use_bias=False)(q_embed)
+        q_text_len = self._params['input_shapes'][0][0]
+        q_text_len = 2 * q_text_len
+
+        q_attention = keras.layers.Lambda(
+            lambda x: softmax(x, axis=1),
+            output_shape=(q_text_len,)
+        )(q_attention)
+        
+        # Score 2
+        score = keras.layers.Dot(axes=[1, 1])([q_attention, q_one_tensors])
+        x_out = self._make_output_layer()(score)
+        
+        self._backend = keras.Model(inputs=[query, doc], outputs=x_out)
