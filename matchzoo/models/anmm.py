4b3a5f45cf0982c4e27958b9380ae33bcce5d802
Deep98 <deep02matrix@gmail.com>
2021-03-18 11:10:19 +0530

    Update anmm.py

--- matchzoo/models/anmm.py
+++ matchzoo/models/anmm.py
@@ -54,7 +54,7 @@ class ANMM(BaseModel):
         # query is [batch_size, left_text_len]
         # doc is [batch_size, right_text_len, bin_num]
         query, doc, freq_vec, d_one_tensors, q_one_tensors = self._make_inputs()
-        embedding = self._make_embedding_layer()
+        #embedding = self._make_embedding_layer()
         
         d_bin0 = tensorflow.keras.layers.Dropout(
             rate=self._params['dropout_rate'])(doc)
@@ -99,7 +99,7 @@ class ANMM(BaseModel):
             rate=self._params['dropout_rate'])(pos_vec)
         
         #q_embed1 = tensorflow.keras.layers.Reshape((q_embed0,))(q_embed1)
-        '''
+        
         q_embed0 = embedding(query)
         #q_embed0 = tensorflow.keras.layers.Dropout(
         #    rate=self._params['dropout_rate'])(query)
@@ -108,7 +108,7 @@ class ANMM(BaseModel):
         #q_embed0 = embedding(q_embed0)
         q_embed1 = embedding(score0)
         #q_embed = tensorflow.keras.layers.Concatenate()([q_embed0, q_embed1])
-        '''
+        
         q_attention = tensorflow.keras.layers.Dense(
             1, kernel_initializer=RandomUniform(), use_bias=False)(q_embed0)
         q_text_len = self._params['input_shapes'][0][0]
@@ -117,7 +117,7 @@ class ANMM(BaseModel):
             lambda x: softmax(x, axis=1),
             output_shape=(q_text_len,)
         )(q_attention)
-        '''
+        
         
         for layer_id in range(self._params['num_layers'] - 1):
             q_embed0 = tensorflow.keras.layers.Dense(
@@ -131,6 +131,35 @@ class ANMM(BaseModel):
         
         # Score 2
         score = tensorflow.keras.layers.Dot(axes=[1, 1])([q_embed0, q_embed1])
+        x_out = self._make_output_layer()(score)
+        '''
+        
+        q_embed0 = tensorflow.keras.layers.Dropout(
+            rate=self._params['dropout_rate'])(query)
+            
+        #frequency vector
+        
+        q_embed1 = tensorflow.keras.layers.Dropout(
+            rate=self._params['dropout_rate'])(score0)
+        
+        #d_bin1 = tensorflow.cast(d_bin1, tensorflow.int32)
+        
+        #d_bin1 = tensorflow.keras.layers.Reshape((d_bin0,))(d_bin1)
+            
+        q_embed = tensorflow.keras.layers.Concatenate()([q_embed0, q_embed1])
+        
+        for layer_id in range(self._params['num_layers'] - 1):
+            q_embed = tensorflow.keras.layers.Dense(
+                self._params['hidden_sizes'][layer_id],
+                kernel_initializer=RandomUniform())(q_embed)
+            q_embed = tensorflow.keras.layers.Activation('tanh')(q_embed)
+        
+        q_embed = tensorflow.keras.layers.Dense(
+            self._params['hidden_sizes'][self._params['num_layers'] - 1])(
+            q_embed)
+        
+        # Score 1
+        score = tensorflow.keras.layers.Dot(axes=[1, 1])([q_embed, q_one_tensors])
         x_out = self._make_output_layer()(score)
         
         self._backend = tensorflow.keras.Model(inputs=[query, doc, freq_vec, d_one_tensors, q_one_tensors], outputs=x_out)
